b482d69ceb295b3345fd1874802af6c323e805d6
Gabriel Laroumanie <g.laroumanie@gmail.com>
2026-02-04 14:16:56 +0100

    fix : eslint warning

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+
 import { AuthController } from './auth.controller';
 
 @Module({
@@ -9,16 +10,16 @@ import { AuthController } from './auth.controller';
     ConfigModule.forRoot(),
     ClientsModule.registerAsync([
       {
-        name: 'AUTH_SERVICE',
         imports: [ConfigModule],
         inject: [ConfigService],
+        name: 'AUTH_SERVICE',
         useFactory: (config: ConfigService) => ({
-          transport: Transport.RMQ,
           options: {
-            urls: [config.get<string>('RABBITMQ_URL', 'amqp://guest:guest@localhost:5672')],
             queue: 'auth_queue',
             queueOptions: { durable: false },
+            urls: [config.getOrThrow<string>('RABBITMQ_URL')],
           },
+          transport: Transport.RMQ,
         }),
       },
     ]),
