dccf06fb7dd196d99d3059a6ec908af865d3cd90
alexis <alexisvasseur96@gmail.com>
2026-03-16 11:58:10 +0100

    ci: fix lint and tests

--- packages/jest-config/src/nest.ts
+++ packages/jest-config/src/nest.ts
@@ -1,14 +1,28 @@
 import type { Config } from 'jest';
 import { config as baseConfig } from './base';
 
-export const nestConfig = {
+const sharedNestConfig = {
     ...baseConfig,
-    rootDir: 'src',
-    testRegex: '.*\\.spec\\.ts$',
     transform: {
         '^.+\\.(t|j)s$': 'ts-jest',
     },
+    moduleNameMapper: {
+        '^@repo/(.+)$': '<rootDir>/../../packages/$1/src',
+    },
+    testEnvironment: 'node',
+} satisfies Config;
+
+export const nestConfig = {
+    ...sharedNestConfig,
+    rootDir: 'src',
+    testRegex: '.*\\.spec\\.ts$',
     collectCoverageFrom: ['**/*.(t|j)s'],
     coverageDirectory: '../coverage',
-    testEnvironment: 'node',
+} as const satisfies Config;
+
+export const nestE2eConfig = {
+    ...sharedNestConfig,
+    rootDir: '.',
+    testRegex: '.e2e-spec.ts$',
+    passWithNoTests: true,
 } as const satisfies Config;
